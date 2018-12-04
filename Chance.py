e9bb4e1e2cfb7ff293dd0517e04a713bc2bdb653
Flossano <43013492+Flossano@users.noreply.github.com>
2018-12-04 14:42:02 -0500

    Add files via upload

---
+++ Chance.py
@@ -0,0 +1,67 @@
+import random
+
+class chanceCard:
+    
+    #type - are you moving player position or affecting bank account
+    #effect - by how much
+    
+    def __init__(self,type,effect):
+        
+        self.type = type
+        self.effect = effect
+    
+    def __str__(self):
+        
+        return "Chance Card (%s,%s)" % (self.type,self.effect)
+
+
+#Chance is considered to be "Movement Deck" so more movement cards
+class Chance:
+    
+    RAILROADS = [6,16,26,36]
+    
+    UTILTIES = [13,29]
+    
+    CASH = [10,20,30,40,50,60,70,80,90,100,150,200,-10,-20,-30,-40,-50,-60,-70,-80,-90,-100,-150,-200]
+    
+    CARDS = [chanceCard('go To', random.randint(2,40)),
+             chanceCard('go To', random.randint(2,40))
+             chanceCard('go To', random.randint(2,40))
+             chanceCard('go To', random.randint(2,40))
+             chanceCard('Advance to GO', 1),
+             chanceCard('Go To Jail', 31),
+             chanceCard('Go To Free Parking', 21),
+             chanceCard('Go To RailRoads', random.choice(RAILROADS)),
+             chanceCard('Go to BoardWalk', 40),
+             chanceCard('cash',random.choice(CASH)),
+             chanceCard('give', 10), #to every other player
+             chanceCard('receive', 10) #to every other player
+             ]
+        
+    def __init__(self):
+                 
+        random.shuffle(self.CARDS)
+
+
+    def selectCardforCurrentPlayer(self):
+    
+        currentPlayersCard = self.CARDS[0]
+        
+        random.shuffle(self.CARDS)
+        
+        return currentPlayersCard
+
+#tester
+if __name__ == "__main__":
+    
+    x = Chance()
+    
+    playerCard = x.selectCardforCurrentPlayer()
+    
+    print(playerCard)
+
+
+
+
+
+
--- communityChest.py
+++ communityChest.py
@@ -14,13 +14,19 @@ class communityChestCard:
         
         return "Community Card (%s,%s)" % (self.type,self.effect)
 
-
+#communityChest considered to be "Money Deck" so more money cards
 class communityChest:
     
     CASH = [10,20,30,40,50,60,70,80,90,100,150,200,-10,-20,-30,-40,-50,-60,-70,-80,-90,-100,-150,-200]
     
     CARDS = [communityChestCard('go To', random.randint(2,40)),
-             communityChestCard('Advance to GO', 0),
+             communityChestCard('Advance to GO', 1),
+             communityChestCard('cash',random.choice(CASH)),
+             communityChestCard('cash',random.choice(CASH)),
+             communityChestCard('cash',random.choice(CASH)),
+             communityChestCard('cash',random.choice(CASH)),
+             communityChestCard('give', 10), #to every other player
+             communityChestCard('receive', 10) #to every other player
              communityChestCard('cash',random.choice(CASH)),
              communityChestCard('give', 10), #to every other player
              communityChestCard('receive', 10) #to every other player
@@ -39,7 +45,7 @@ class communityChest:
 
         return currentPlayersCard
 
-
+#tester
 if __name__ == "__main__":
 
     x = communityChest()
