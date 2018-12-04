58cb3defd24f2886286da41f4d2cfa214885d343
MeierNicholas <43380409+MeierNicholas@users.noreply.github.com>
2018-12-04 09:37:59 -0500

    Update monopolyGUI.py
    added comment about monopoly.jpg file

--- monopolyGUI.py
+++ monopolyGUI.py
@@ -3,6 +3,7 @@ import thorpy
 
 if __name__ == "__main__":
     application = thorpy.Application(size=(752, 754), caption="Monopoly")
+    #Make sure 'monopoly.jpg' is in the same working directory
     background = thorpy.Background(image=R"monopoly.jpg")
     menu = thorpy.Menu(background)
     menu.play()
