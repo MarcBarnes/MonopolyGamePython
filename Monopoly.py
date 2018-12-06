0aa209f121f36eb81818c284e9cfea2c1af66943
marcobarnes <marcobarnes@gmail.com>
2018-12-06 12:15:29 -0500

    integrating button and nick

--- Monopoly.py
+++ Monopoly.py
@@ -46,8 +46,9 @@ class button():
 
 class Player:
     def __init__(self, number):
-        self.position = random.randint(1,40)
+        self.position = 1
         self.number = number
+        self.isPlayerBankrupt = False
 
         if self.number == 1:
             self.pawnColor = PLAYERCOLOR[0]
@@ -78,16 +79,126 @@ class Player:
             return numTurns
 
 
+class makeNewBoard:
+    def __init__(self, myList):
+        width, height = 752, 754
+        self.screen = pygame.display.set_mode((width, height))
+
+        bg = pygame.image.load("monopoly.jpg")
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                pos = pygame.mouse.get_pos()
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:    #start has been pushed
+                    if startButton.isOver(pos):
+                        print("Game has started")
+
+            pygame.display.update()
+
+            self.screen.blit(bg, [0,0])
+            pygame.display.flip()
+
+            for p in myList:
+                if p.position == 1:
+                    a1 = pygame.draw.circle(self.screen, p.pawnColor, (700, 700), 20, 0)
+                if p.position == 2:
+                    a2 = pygame.draw.circle(self.screen, p.pawnColor, (630, 700), 20, 0)
+                if p.position == 3:
+                    a3 = pygame.draw.circle(self.screen, p.pawnColor, (565, 700), 20, 0)
+                if p.position == 4:
+                    a4 = pygame.draw.circle(self.screen, p.pawnColor, (500, 700), 20, 0)
+                if p.position == 5:
+                    a5 = pygame.draw.circle(self.screen, p.pawnColor, (440, 700), 20, 0)
+                if p.position == 6:
+                    a6 = pygame.draw.circle(self.screen, p.pawnColor, (380, 700), 20, 0)
+                if p.position == 7:
+                    a7 = pygame.draw.circle(self.screen, p.pawnColor, (315, 700), 20, 0)
+                if p.position == 8:
+                    a8 = pygame.draw.circle(self.screen, p.pawnColor, (255, 700), 20, 0)
+                if p.position == 9:
+                    a9 = pygame.draw.circle(self.screen, p.pawnColor, (190, 700), 20, 0)
+                if p.position == 10:
+                    a10 = pygame.draw.circle(self.screen, p.pawnColor, (130, 700), 20, 0)
+                if p.position == 11:
+                    a11 = pygame.draw.circle(self.screen, p.pawnColor, (60, 700), 20, 0)
+                if p.position == 12:
+                    a12 = pygame.draw.circle(self.screen, p.pawnColor, (60, 630), 20, 0)
+                if p.position == 13:
+                    a13 = pygame.draw.circle(self.screen, p.pawnColor, (60, 565), 20, 0)
+                if p.position == 14:
+                    a14 = pygame.draw.circle(self.screen, p.pawnColor, (60, 505), 20, 0)
+                if p.position == 15:
+                    a15 = pygame.draw.circle(self.screen, p.pawnColor, (60, 445), 20, 0)
+                if p.position == 16:
+                    a16 = pygame.draw.circle(self.screen, p.pawnColor, (60, 380), 20, 0)
+                if p.position == 17:
+                    a17 = pygame.draw.circle(self.screen, p.pawnColor, (60, 315), 20, 0)
+                if p.position == 18:
+                    a18 = pygame.draw.circle(self.screen, p.pawnColor, (60, 250), 20, 0)
+                if p.position == 19:
+                    a19 = pygame.draw.circle(self.screen, p.pawnColor, (60, 185), 20, 0)
+                if p.position == 20:
+                    a20 = pygame.draw.circle(self.screen, p.pawnColor, (60, 120), 20, 0)
+                if p.position == 21:
+                    a21 = pygame.draw.circle(self.screen, p.pawnColor, (60, 50), 20, 0)
+                if p.position == 22:
+                    a22 = pygame.draw.circle(self.screen, p.pawnColor, (125, 50), 20, 0)
+                if p.position == 23:
+                    a23 = pygame.draw.circle(self.screen, p.pawnColor, (190, 50), 20, 0)
+                if p.position == 24:
+                    a24 = pygame.draw.circle(self.screen, p.pawnColor, (250, 50), 20, 0)
+                if p.position == 25:
+                    a25 = pygame.draw.circle(self.screen, p.pawnColor, (310, 50), 20, 0)
+                if p.position == 26:
+                    a26 = pygame.draw.circle(self.screen, p.pawnColor, (375, 50), 20, 0)
+                if p.position == 27:
+                    a27 = pygame.draw.circle(self.screen, p.pawnColor, (440, 50), 20, 0)
+                if p.position == 28:
+                    a28 = pygame.draw.circle(self.screen, p.pawnColor, (505, 50), 20, 0)
+                if p.position == 29:
+                    a29 = pygame.draw.circle(self.screen, p.pawnColor, (570, 50), 20, 0)
+                if p.position == 30:
+                    a30 = pygame.draw.circle(self.screen, p.pawnColor, (630, 50), 20, 0)
+                if p.position == 31:
+                    a31 = pygame.draw.circle(self.screen, p.pawnColor, (700, 50), 20, 0)
+                if p.position == 32:
+                    a32 = pygame.draw.circle(self.screen, p.pawnColor, (700, 125), 20, 0)
+                if p.position == 33:
+                    a33 = pygame.draw.circle(self.screen, p.pawnColor, (700, 195), 20, 0)
+                if p.position == 34:
+                    a34 = pygame.draw.circle(self.screen, p.pawnColor, (700, 255), 20, 0)
+                if p.position == 35:
+                    a35 = pygame.draw.circle(self.screen, p.pawnColor, (700, 315), 20, 0)
+                if p.position == 36:
+                    a36 = pygame.draw.circle(self.screen, p.pawnColor, (700, 375), 20, 0)
+                if p.position == 37:
+                    a37 = pygame.draw.circle(self.screen, p.pawnColor, (700, 440), 20, 0)
+                if p.position == 38:
+                    a38 = pygame.draw.circle(self.screen, p.pawnColor, (700, 505), 20, 0)
+                if p.position == 39:
+                    a39 = pygame.draw.circle(self.screen, p.pawnColor, (700, 560), 20, 0)
+                if p.position == 40:
+                    a40 = pygame.draw.circle(self.screen, p.pawnColor, (700, 625), 20, 0)
+
+            pygame.display.flip()
+
 
 if __name__ == "__main__":
-    # players = []                                        #array of players
-    # playerOne = Player(1)
-    # playerTwo = Player(2)
-    # playerThree = Player(3)
-    # playerFour = Player(4)
+    playerOne = Player(1)
+    playerTwo = Player(2)
+    playerThree = Player(3)
+    playerFour = Player(4)
+
+    myList = [playerOne, playerTwo, playerThree, playerFour]
+
     width, height = 752, 800    #set size of the window. The size of the image is 752,754 but I went to 800 to add space for buttons
     screen = pygame.display.set_mode((width, height))
     startButton = button((0, 255, 255), 752/2 - 50, 754/2 - 50, 100, 50, "Start")
+    moveButton = button((0, 255, 255), 752/2 - 50, 754/2 - 50, 100, 50, "Move")
     bg = pygame.image.load("monopoly.jpg")
     gameHasStarted = False                              #initialize that the game hasnt started
     numPlayersChosen = False                            #initialize that number of players hasnt been chosen
@@ -97,23 +208,39 @@ if __name__ == "__main__":
     print("Started Running main loop")
     while running:
         screen.blit(bg, [0, 0])                         #paint the backgorund image in the back first so it doesnt cover anything
-        print("Painting Background")                    #debug comment
+        #print("Painting Background")                    #debug comment
+        moveBtnClicked = False
         for event in pygame.event.get():                #end python interpretter if window has been closed
             pos = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
                 running = False
                 quit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:    #start has been pushed
+            if event.type == pygame.MOUSEBUTTONDOWN and gameHasStarted == False:    #start has been pushed
                 if startButton.isOver(pos):
                     gameHasStarted = True
                     print("Game has started")
+
+            if event.type == pygame.MOUSEBUTTONDOWN and gameHasStarted == True:
+                if startButton.isOver(pos):
+                    moveBtnClicked = True
+                    #call movePawn() function
+                    makeNewBoard(myList)
+
+                    playerOne.position = 2
+                    playerTwo.position = 5
+                    playerThree.position = 10
+                    playerFour.position = 15
+                    makeNewBoard(myList)
+
+                    print("move button clicked")
 
         if gameHasStarted == False:
             print("Drawing start button")
             startButton.draw(screen)
 
         if gameHasStarted:
+            moveButton.draw(screen)
             if numPlayersChosen == False:
                 print("Drawing howManyPlayers text")
                 pressedKey = pygame.key.get_pressed()
@@ -136,17 +263,17 @@ if __name__ == "__main__":
 
                 players = [Player(1), Player(2), Player(3), Player(4)]
                 if numPlayers == 2:
-                    startpos1 = pygame.draw.circle(screen, PLAYERCOLOR[0], (620, 650), 20, 0)
-                    startpos2 = pygame.draw.circle(screen, PLAYERCOLOR[1], (620, 620), 20, 0)
+                    startpos1 = pygame.draw.circle(screen, PLAYERCOLOR[0], (690, 680), 20, 0)
+                    startpos2 = pygame.draw.circle(screen, PLAYERCOLOR[1], (690, 650), 20, 0)
                 elif numPlayers == 3:
-                    startpos1 = pygame.draw.circle(screen, PLAYERCOLOR[0], (620, 650), 20, 0)
-                    startpos2 = pygame.draw.circle(screen, PLAYERCOLOR[1], (620, 620), 20, 0)
-                    startpos3 = pygame.draw.circle(screen, PLAYERCOLOR[2], (620, 590), 20, 0)
+                    startpos1 = pygame.draw.circle(screen, PLAYERCOLOR[0], (690, 680), 20, 0)
+                    startpos2 = pygame.draw.circle(screen, PLAYERCOLOR[1], (690, 650), 20, 0)
+                    startpos3 = pygame.draw.circle(screen, PLAYERCOLOR[2], (690, 620), 20, 0)
                 if numPlayers == 4:
-                    startpos1 = pygame.draw.circle(screen, PLAYERCOLOR[0], (620, 650), 20, 0)
-                    startpos2 = pygame.draw.circle(screen, PLAYERCOLOR[1], (620, 620), 20, 0)
-                    startpos3 = pygame.draw.circle(screen, PLAYERCOLOR[2], (620, 590), 20, 0)
-                    startpos4 = pygame.draw.circle(screen, PLAYERCOLOR[3], (620, 560), 20, 0)
+                    startpos1 = pygame.draw.circle(screen, PLAYERCOLOR[0], (690, 680), 20, 0)
+                    startpos2 = pygame.draw.circle(screen, PLAYERCOLOR[1], (690, 650), 20, 0)
+                    startpos3 = pygame.draw.circle(screen, PLAYERCOLOR[2], (690, 620), 20, 0)
+                    startpos4 = pygame.draw.circle(screen, PLAYERCOLOR[3], (690, 590), 20, 0)
                 players[0].takeTurn(totalTurnsTaken)
 
 
