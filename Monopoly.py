b75a42f0acb134bf66ebb2c1d36808f0ff8a9b7e
sdr14b <sdr14b@my.fsu.edu>
2018-12-06 22:22:20 -0500

    Revert "stuff"
    This reverts commit 10bbbe184ecc5f380eabb293be85c69c0d4e97be.

--- Monopoly.py
+++ Monopoly.py
@@ -78,7 +78,6 @@ class button():
 class Player:
     def __init__(self, number):
         self.position = 1
-        self.isPlayerBankrupt = False
         self.number = number
         self.money = 1500
         self.numProperties = 0
@@ -309,7 +308,7 @@ class Player:
                                         if pay75.isOver(pos):
                                             self.money = self.money - 75
                                             print("Player ", self.number, "has", self.money, "Player paid $75")
-                                            waitingToPayLux = False
+                                            waitingToPay = False
 
 
                         self.takingTurn = False
@@ -341,10 +340,6 @@ class Player:
                             if EstateDict[estate]["estateName"] == property.text.split(':')[0]:
                                 print(tempButton.text)
                                 propertyIndex = estate
-
-                        if int(EstateDict[propertyIndex]["houses"]) > 3:
-                            jayarama = False
-                            break
 
                         if players[theBoard.totalTurnsTaken % theBoard.numPlayers].money >= int(EstateDict[estate]["houseCost"]):
                             print(propertyIndex)
@@ -514,7 +509,6 @@ class Player:
                         return False
 
                 else:
-                    self.payRent()
                     return False
 
     def checkForJail(self):
@@ -538,31 +532,10 @@ class Player:
         while self.playerHasPaid == False:
             if self.isPlayerBankrupt:
                 return False
-            elif self.number == int(EstateDict[self.position]['ownerNumber']):  # they own it
+            elif self.number == EstateDict[self.position]['ownerNumber']:  # they own it
                 return True
-            elif int(EstateDict[self.position]['rent']) <= self.money and int(EstateDict[self.position]['monopoly']) == 0:      # they don't own it but they have the money to pay for it and no monopoly
-                numHOUSES = EstateDict[self.position]['houses']
-                playerToPay = EstateDict[self.position]['ownerNumber']
-                if numHOUSES == 0:
-                    self.money = self.money - EstateDict[self.position]['rent']
-                    players[playerToPay].money = players[playerToPay].money + EstateDict[self.position]['rent']
-
-                elif numHOUSES == 1:
-                    self.money = self.money - EstateDict[self.position]['own']
-                    players[playerToPay].money = players[playerToPay].money + EstateDict[self.position]['own1HouseRent']
-
-                elif numHOUSES == 2:
-                    self.money = self.money - EstateDict[self.position]['rent']
-                    players[playerToPay].money = players[playerToPay].money + EstateDict[self.position]['own2HouseRent']
-
-                elif numHOUSES == 3:
-                    self.money = self.money - EstateDict[self.position]['rent']
-                    players[playerToPay].money = players[playerToPay].money + EstateDict[self.position]['own3HouseRent']
-
-                elif numHOUSES == 4:
-                    self.money = self.money - EstateDict[self.position]['rent']
-                    players[playerToPay].money = players[playerToPay].money + EstateDict[self.position]['own4HouseRent']
-
+            elif EstateDict[self.position]['rent'] <= self.money and EstateDict[self.position]['monopoly'] == 0:      # they don't own it but they have the money to pay for it and no monopoly
+                self.money = self.money - EstateDict[self.position]['rent']
                 return True
             elif EstateDict[self.position]['monopoly'] == 1:
                 if self.money >= EstateDict[self.position]['price'] * 2:
